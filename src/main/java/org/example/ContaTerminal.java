5d175fb551d78d6a79a1203daecb122c84455677
marcelinoShion <marcelinoshion@gmail.com>
2024-10-09 11:24:47 -0300

    update

--- src/main/java/org/example/ContaTerminal.java
+++ src/main/java/org/example/ContaTerminal.java
@@ -1,17 +1,41 @@
 package org.example;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ContaTerminal {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        boolean ativo = true ;
+        int numero ;
+        String agencia ;
+        String nomeDoCliente;
+        Double saldo ;
+        int escolha;
+        while(ativo) {
+            try {
+                System.out.println("Insira o numero da Conta");
+                 numero = scanner.nextInt();
+                System.out.println("Insira o numero da Agencia");
+                agencia =  scanner.next();
+                System.out.println("Insira o nome do cliente");
+                nomeDoCliente = scanner.next();
+                System.out.println("Insira o Saldo");
+                saldo = scanner.nextDouble();
+                System.out.println("Ola "+nomeDoCliente+ ", obrigado por criar uma conta em nosso banco, sua agencia é "+agencia+" conta "+ numero
+                        +" e seu saldo "+ saldo + " ja disponivel");
+                System.out.println("Digite 1 para continuar");
+                escolha = scanner.nextInt();
+                ativo = escolha == 1 ? true : false ;
+            } catch (InputMismatchException e) {
+                System.out.println("Error :" + e.getMessage());
+            } catch (Exception e){
+                System.out.println("Error :" + e.getMessage());
+            }finally {
+                scanner.next();
+            }
         }
     }
 }
